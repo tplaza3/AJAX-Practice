ce3b56e3e83520d1674edef0ee33363fb7bab3e6
Tony Plaza <tplaza3@gmail.com>
2014-09-25 15:22:58 -0500

    add initial files

---
+++ AJAX/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+AJAX::Application.config.secret_token = '8ed5a0fec1f38f6938fb584bd2b590f4b6feac9e26ca0782e71c3266675f5f39b2cbfbd913ddb601af7ae0b28483a6935634c80345b69daf83654d55ed3da98d'
